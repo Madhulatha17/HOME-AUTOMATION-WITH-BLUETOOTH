b22ee449850e01f0327f1af766a13eaa64cd0aba
Madhulatha17 <bandimadhulatha4@gmail.com>
2026-06-21 12:57:46 +0530

    Create home_automation_bluetooth.ino

---
+++ home_automation_bluetooth.ino
@@ -0,0 +1,22 @@
+char data;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    data = Serial.read();
+
+    if (data == '1') {
+      digitalWrite(13, HIGH);
+      Serial.println("Device ON");
+    }
+
+    if (data == '0') {
+      digitalWrite(13, LOW);
+      Serial.println("Device OFF");
+    }
+  }
+}
